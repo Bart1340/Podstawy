a2515262b4edb796d1da003eaea1c2ae6186100f
Bartosz Cieśla <117359950+Bart1340@users.noreply.github.com>
2023-05-30 22:38:14 +0200

    Update and rename Dice rolls1.R to Dice rolls.R

--- R/Dice rolls.R
+++ R/Dice rolls.R
@@ -1,3 +1,4 @@
+###1
 Dice_rolls <- 0      #ustawiamy liczbę rzutów na 0 
 while (Dice_rolls<100){     #dopóki liczba rzutów jest mniejsza od 100 to:
   Dice_rolls <- Dice_rolls+1    #licznik rzutów 
@@ -12,6 +13,7 @@ while (Dice_rolls<100){     #dopóki liczba rzutów jest mniejsza od 100 to:
   }
 } 
 
+###2
 Sum_Dice_Rolls <- 0   #ustawiamy sumę rzutów kostką na 0
 Iteration_number <- 0   #ustawiamy liczbę iteracji na 0
 while (Sum_Dice_Rolls<=1001) {   #dopóki suma rzutów kostką nie przekroczy 1000
@@ -21,3 +23,21 @@ while (Sum_Dice_Rolls<=1001) {   #dopóki suma rzutów kostką nie przekroczy 10
 }
 print(Sum_Dice_Rolls)
 print(Iteration_number)
+
+
+###3
+for (i in 1:100) {  #Dla każdej kolejnej iteracji "i" aż do 100
+  dice_rolls_to_sum <- i-1   #rzuty kostką do zsumowania o 1 mniejsze od liczby iteracji
+  sum <- 0 #  Na początku każdej iteracji sumę rzutów ustawiamy na 0 
+  if (dice_rolls_to_sum >= 1) {   #Jeżeli liczba rzutów do zsumowania jest większa od 0
+    for (dice_roll in 1:dice_rolls_to_sum) {  #Dla każdego kolejnego rzutu kostką w zbiorze rzutów do zsumowania
+      if((i %% 2) == 0)  #Jeżeli liczba iteracji jest parzysta to rzut kostką dwunastościenną 
+        x <-  {sample(1:12,1)
+        } else   #Jeżeli liczba iteracji jest nieparzysta to rzut kostką sześciościenną 
+          x <- {sample(1:6,1)
+          }  
+        sum <- sum + x   #Suma rzutów kostką w danej iteracji      
+    }
+  }
+  cat("Iteracja",i,"-","suma rzutów",sum, "\n")
+}
