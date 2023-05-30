7c29c23943360291c8fd7afa93b217b4dfc85b7a
Bartosz Cieśla <117359950+Bart1340@users.noreply.github.com>
2023-05-30 22:39:47 +0200

    Update While&Repeat.R

--- R/While&Repeat.R
+++ R/While&Repeat.R
@@ -36,4 +36,25 @@ repeat {
   }
 }
 
+#3przykład
+x <- 0
+warunek_logiczny <- TRUE
+while(warunek_logiczny) {
+  x <- sample(1:6, 1)
+  print(x)
+  if (x==6) {
+    warunek_logiczny <- FALSE
+  }
+}
 
+x <- 0
+warunek_logiczny <- TRUE
+repeat {
+  if (!warunek_logiczny) {
+    break
+  }
+  x <- sample(1:6, 1)
+  print(x)
+  if (x==6) {
+    warunek_logiczny <- FALSE
+  }
